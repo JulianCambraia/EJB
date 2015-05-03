dac5330ccc1de801375ee16982d539cb0627205f
JulianFernando <juliancambraia@gmail.com>
2015-05-03 15:46:56 -0300

    Adiciona Interface ao EJB Cliente

---
+++ EJB/calculadoraClient/ejbModule/br/com/julianfernando/sessionbeans/CalculadoraIf.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package br.com.julianfernando.sessionbeans;
+
+/**
+ * @author julian fernando
+ * Interface que define as operações do SLSB
+ *
+ */
+public interface CalculadoraIf {
+	
+	double soma(double a, double b);
+}
